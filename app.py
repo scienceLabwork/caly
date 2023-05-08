b25a115f782c389a4490181307268cd75b18a9c9
Pankti <panktichawda06@gmail.com>
2023-05-08 14:34:33 +0530

    pankti

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ port = int(os.environ.get("PORT", 5000))
 def index():
     return render_template('index.html')
 
+@app.route('/landing')
+def landing():
+    return render_template('landing.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
